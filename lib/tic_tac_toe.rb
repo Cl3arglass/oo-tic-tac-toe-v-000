16f1020a0b7709dcee223e2ef2567bdc80d3e311
Holden Altaffer <haltaffer@gmail.com>
2018-02-16 10:13:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,8 +67,8 @@ class TicTacToe
   #   turns
   end
 
-  def current_player(board)
-    turn_count(board)  % 2 == 0 ? "X" : "O"
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
   end
 
   def won?(board)
