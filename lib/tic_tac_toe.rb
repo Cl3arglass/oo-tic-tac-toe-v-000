c4a0a069c7e91fdefacab512587d6731ec9198cd
Holden Altaffer <haltaffer@gmail.com>
2018-02-16 10:12:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,9 +50,9 @@ class TicTacToe
     end
   end
 
-  def turn_count(board)
+  def turn_count
   count = 0
-    board.each do |space|
+    @board.each do |space|
       if space != " "
       count += 1
     end
